a776ad653941c93d8656b2a2894e84c4ef83a84b
Adam Fowler <adamfowler71@gmail.com>
2026-06-10 18:16:24 +0100

    Add file headers

--- Sources/hb/ValidationRules.swift
+++ Sources/hb/ValidationRules.swift
@@ -1,10 +1,18 @@
+//
+// This source file is part of the Hummingbird server framework project
+// Copyright (c) the Hummingbird authors
+//
+// See LICENSE.txt for license information
+// SPDX-License-Identifier: Apache-2.0
+//
+
 import Noora
 
 struct NoWhitespaceValidationRule: ValidatableRule {
     let error: any ValidatableError
 
     func validate(input: String) -> Bool {
-        return !input.contains(where: \.isWhitespace)
+        !input.contains(where: \.isWhitespace)
     }
 }
 
@@ -12,6 +20,6 @@ struct AsciiValidationRule: ValidatableRule {
     let error: any ValidatableError
 
     func validate(input: String) -> Bool {
-        return !input.contains(where: { !$0.isASCII })
+        !input.contains(where: { !$0.isASCII })
     }
 }
