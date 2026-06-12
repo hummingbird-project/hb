632f0dab69492556e8a0a3db383a6c10c93d6dac
Adam Fowler <adamfowler71@gmail.com>
2026-06-12 13:56:34 +0100

    Use readableBytesView instead of creating Data

--- Sources/hb/Commands/InitCommand.swift
+++ Sources/hb/Commands/InitCommand.swift
@@ -200,7 +200,7 @@ struct InitCommand: AsyncParsableCommand {
             }
         let response = try await HTTPClient.shared.get(url: "\(url)/archive/refs/tags/\(version).zip").get()
         guard let responseBody = response.body else { throw HBError("Failed to download release \(version)") }
-        return try ZipArchiveReader(buffer: Data(buffer: responseBody))
+        return try ZipArchiveReader(buffer: responseBody.readableBytesView)
     }
 
     func createZipFromFolder(_ folder: FilePath) throws -> ZipArchiveReader<some ZipReadableStorage> {
